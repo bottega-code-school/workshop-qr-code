1db0ad9d77457da1fdede5c734e53ca4eff4c771
Jordan Hudgens <jordan@devcamp.com>
2021-09-01 14:03:55 -0700

    Built out initial layout for the qr code system

--- src/components/QrCode.tsx
+++ src/components/QrCode.tsx
@@ -3,5 +3,21 @@ import * as React from "react";
 const icon = require("../../static/assets/images/code-icon.png");
 
 export default function QrCode() {
-  return <div>QR Code here</div>;
+  const [url, setUrl] = React.useState("");
+
+  return (
+    <div className="qr-container">
+      <form className="qr-container__form">
+        <input
+          type="text"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+
+        <button type="submit">Generate QR Code</button>
+      </form>
+
+      <div className="qr-container__qr-code">Code goes here...</div>
+    </div>
+  );
 }
