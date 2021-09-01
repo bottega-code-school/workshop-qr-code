93dbc5fd0c68ade23a1eb1ec129619ea8d0ff9c3
Jordan Hudgens <jordan@devcamp.com>
2021-09-01 14:28:57 -0700

    Implemented the ability to download

--- src/components/QrCode.tsx
+++ src/components/QrCode.tsx
@@ -1,23 +1,59 @@
 import * as React from "react";
+import QRCode from "qrcode.react";
 
 const icon = require("../../static/assets/images/code-icon.png");
 
 export default function QrCode() {
+  const qrRef = React.useRef();
   const [url, setUrl] = React.useState("");
+
+  const downloadQRCode = (evt: React.FormEvent) => {
+    evt.preventDefault();
+    // @ts-ignore
+    let canvas = qrRef.current.querySelector("canvas");
+    let image = canvas.toDataURL("image/png");
+    let anchor = document.createElement("a");
+    anchor.href = image;
+    anchor.download = `qr-code.png`;
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+    setUrl("");
+  };
+
+  const qrCode = (
+    <QRCode
+      id="qrCodeElToRender"
+      size={500}
+      value={url}
+      bgColor="white"
+      fgColor="#141926"
+      level="H"
+      imageSettings={{
+        src: icon,
+        excavate: true,
+        width: 500 * 0.1,
+        height: 500 * 0.1,
+      }}
+    />
+  );
 
   return (
     <div className="qr-container">
-      <form className="qr-container__form">
+      <form onSubmit={downloadQRCode} className="qr-container__form">
         <input
           type="text"
           value={url}
           onChange={(e) => setUrl(e.target.value)}
+          placeholder="https://example.com"
         />
 
-        <button type="submit">Generate QR Code</button>
+        <button type="submit">Download QR Code</button>
       </form>
 
-      <div className="qr-container__qr-code">Code goes here...</div>
+      <div className="qr-container__qr-code" ref={qrRef}>
+        {qrCode}
+      </div>
     </div>
   );
 }
