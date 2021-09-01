9f49ef965e14195aacfdc45241f773b5342a9506
Jordan Hudgens <jordan@devcamp.com>
2021-09-01 14:30:58 -0700

    Implemented initial code for qr code starter

--- src/components/QrCode.tsx
+++ src/components/QrCode.tsx
@@ -4,39 +4,14 @@ import QRCode from "qrcode.react";
 const icon = require("../../static/assets/images/code-icon.png");
 
 export default function QrCode() {
-  const qrRef = React.useRef();
   const [url, setUrl] = React.useState("");
 
   const downloadQRCode = (evt: React.FormEvent) => {
     evt.preventDefault();
-    // @ts-ignore
-    let canvas = qrRef.current.querySelector("canvas");
-    let image = canvas.toDataURL("image/png");
-    let anchor = document.createElement("a");
-    anchor.href = image;
-    anchor.download = `qr-code.png`;
-    document.body.appendChild(anchor);
-    anchor.click();
-    document.body.removeChild(anchor);
+    // TODO
+
     setUrl("");
   };
-
-  const qrCode = (
-    <QRCode
-      id="qrCodeElToRender"
-      size={500}
-      value={url}
-      bgColor="white"
-      fgColor="#141926"
-      level="H"
-      imageSettings={{
-        src: icon,
-        excavate: true,
-        width: 500 * 0.1,
-        height: 500 * 0.1,
-      }}
-    />
-  );
 
   return (
     <div className="qr-container">
@@ -51,9 +26,7 @@ export default function QrCode() {
         <button type="submit">Download QR Code</button>
       </form>
 
-      <div className="qr-container__qr-code" ref={qrRef}>
-        {qrCode}
-      </div>
+      <div className="qr-container__qr-code">QR Code here</div>
     </div>
   );
 }
