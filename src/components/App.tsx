de269a07586b241f7da732d7ced399f24a6c23fb
Jordan Hudgens <jordan@devcamp.com>
2021-09-01 13:49:23 -0700

    Built out starter code for the workshop

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,13 @@
 import * as React from "react";
 import Layout from "./Layout";
+import QrCode from "./QrCode";
 
 const App = () => {
-  return <Layout>TODO</Layout>;
+  return (
+    <Layout>
+      <QrCode />
+    </Layout>
+  );
 };
 
 export default App;
---
+++ src/components/QrCode.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const icon = require("../../static/assets/images/code-icon.png");
+
+export default function QrCode() {
+  return <div>QR Code here</div>;
+}
